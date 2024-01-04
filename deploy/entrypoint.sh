64fca69b73346bf33b0af198c2439fe7d22d7ba6
vsaverin <vasiliy.saverin@gmail.com>
2024-01-04 14:59:37 +0600

    fixed entrypoint

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 checkers_parser/manage.py makemigrations
-python3 checkers_parser/manage.py migrate
-python3 checkers_parser/manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:8008
