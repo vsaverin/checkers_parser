095921ec00f7424ebfc5ff47cff14d59dbaad373
vsaverin <vasiliy.saverin@gmail.com>
2024-01-04 14:34:05 +0600

    project run moved to entrypoint

--- checkers_parser/checkers_parser/settings.py
+++ checkers_parser/checkers_parser/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'checkers_parser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
